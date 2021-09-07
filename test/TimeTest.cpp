a501cf63a874766c2987b52791725d49fc5e81bb
Gio-Formichella <gio.formichella@stud.unifi.it>
2021-09-07 17:55:17 +0200

    Time unit test

--- test/TimeTest.cpp
+++ test/TimeTest.cpp
@@ -5,18 +5,34 @@
 #include "gtest/gtest.h"
 #include "../Time.h"
 
-TEST(TimeTest,ConstructorTest){
+TEST(TimeTest,setTime) {
     Time t;
-    ASSERT_TRUE(t.isNull());
+    QString format = "hh:mm:ss";
+
+    ASSERT_NO_THROW(t.setTime(QTime(0,0,10)));
+    ASSERT_EQ(t.showTime(format),"00:00:10");
+
+    ASSERT_NO_THROW(t.setTime(QTime(10,0)));
+    ASSERT_EQ(t.showTime(format),"10:00:00");
+
+    ASSERT_NO_THROW(t.setTime(QTime(0,3)));
+    ASSERT_EQ(t.showTime(format),"00:03:00");
+
+    ASSERT_NO_THROW(t.setTime(QTime(0,0,0,100)));
+
+    ASSERT_THROW(t.setTime(QTime(0,0,66)),std::invalid_argument);
+    ASSERT_THROW(t.setTime(QTime(0,63,50)),std::invalid_argument);
+    ASSERT_THROW(t.setTime(QTime(25,22,22)),std::invalid_argument);
 }
 
-TEST(TimeTest,GetTimeFormatTest) {
+TEST(TimeTest, showTimeTest){
     Time t;
-    ASSERT_EQ("hh:mm:ss", t.getTimeFormat());
-    t.changeTimeFormat();
-    ASSERT_EQ("hh:mm", t.getTimeFormat());
-    t.changeTimeFormat();
-    ASSERT_EQ("H:m:s a", t.getTimeFormat());
-    t.changeTimeFormat();
-    ASSERT_EQ("hh:mm:ss", t.getTimeFormat());
+    t.setTime(QTime(17,48,22));
+    QString format1 = "hh:mm:ss";
+    QString format2 = "hh:mm";
+    QString format3 = "H:m:s a";
+
+    ASSERT_EQ(t.showTime(format1),"17:48:22");
+    ASSERT_EQ(t.showTime(format2),"17:48");
+    ASSERT_EQ(t.showTime(format3),"17:48:22 pm");
 }
