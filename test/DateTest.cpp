21d7f060f1ddc302388c820b82ac7d14121028cf
Gio-Formichella <gio.formichella@stud.unifi.it>
2021-09-07 17:40:20 +0200

    Date unit test

--- test/DateTest.cpp
+++ test/DateTest.cpp
@@ -5,18 +5,28 @@
 #include "gtest/gtest.h"
 #include "../Date.h"
 
-TEST(DateTest,ConstructorTest){
+TEST(DateTest, setDateTest){
     Date d;
-    ASSERT_TRUE(d.isNull());
+    QString format = "dd.MM.yy";
+
+    ASSERT_NO_THROW(d.setDate(QDate(2021,9,7)));
+    ASSERT_EQ(d.showDate(format),"07.09.21");
+
+    ASSERT_NO_THROW(d.setDate(QDate(4,11,27)));
+    ASSERT_EQ(d.showDate(format),"27.11.04");
+
+    ASSERT_THROW(d.setDate(QDate(2021,13,12)),std::invalid_argument);
+    ASSERT_THROW(d.setDate(QDate(2021,2,30)),std::invalid_argument);
 }
 
-TEST(DateTest, GetDateFormatTest) {
+TEST(DateTest, showDateTest){
     Date d;
-    ASSERT_EQ("dd.MM.yyyy", d.getDateFormat());
-    d.changeDateFormat();
-    ASSERT_EQ("dd.MM.yy", d.getDateFormat());
-    d.changeDateFormat();
-    ASSERT_EQ("dddd MMMM d", d.getDateFormat());
-    d.changeDateFormat();
-    ASSERT_EQ("dd.MM.yyyy", d.getDateFormat());
+    QString format1 = "dd.MM.yy";
+    QString format2 = "dddd MMM d";
+    QString format3 = "dd.MM.yyyy";
+    d.setDate(QDate(2021,9,7));
+
+    ASSERT_EQ(d.showDate(format1),"07.09.21");
+    ASSERT_EQ(d.showDate(format2),"Tuesday Sep 7");
+    ASSERT_EQ(d.showDate(format3),"07.09.2021");
 }
